98784245743b5755a06a693a3a44d4a1fce97f64
shekar <shekarraju47@outlook.com>
2023-11-24 19:14:25 +0530

    first commint

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+
+// Connet To DataBase And Like Tabel/Collection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/userDb")
+  .then(() => console.log("Connect SuccessFull"))
+  .catch((e) => console.error("error"));
+
+// Enter The Schema/type(string,num) Of Document Name
+const userScheema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+// Now You can pass the table first and value schema
+const user = mongoose.model("users", userScheema);
+
+// Now You Can do Oparations
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await user.find();
+    res.send(data);
+  } catch (e) {
+    console.error(e.massage);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { name, age } = req.body;
+  try {
+    const data = await user.find({ name: name });
+
+    if (data.length < 1) {
+      const createUser = new user({
+        name: name,
+        age: age,
+      });
+      const result = await createUser.save();
+      res.send(result);
+    } else {
+      res.status(400);
+      res.send({ type: "User Already Exist" });
+    }
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await user.find({ _id: id });
+
+    const idOf = data[0]._id.toString();
+    if (data.length < 1) {
+      res.status(400);
+      res.send({ TypeError: "No Users" });
+    } else {
+      await user.deleteOne({ _id: idOf });
+      res.send({ typeof: "success" });
+    }
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+app.put("/update/:id", async (req, res) => {
+  const userId = req.params.id.toString();
+  const { age, name } = req.body;
+  console.log(age);
+  try {
+    const inDb = await user.find({ _id: userId });
+    if (inDb.length < 1) {
+      res.send({ TypeError: "User not in Db" });
+    } else {
+      const updateUser = await user.updateOne(
+        { _id: userId },
+        { $set: { name: name, age: age } }
+      );
+      res.send({ typeof: "updated" });
+    }
+  } catch (e) {
+    console.error(e.massage);
+  }
+});
+
+app.get("/user/:id", async (req, res) => {
+  const useId = req.params.id;
+  console.log(useId);
+  try {
+    const data = await user.findOne({ _id: useId });
+    res.send(data);
+  } catch (e) {
+    console.error(e.massage);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("server started at http://localhost:5000/");
+});
